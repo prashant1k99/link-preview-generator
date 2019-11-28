5888db02a3b1ed33fa1fa5d39aa63c92a2b10940
Glitch (hello-express) <none>
2019-11-28 09:35:55 +0000

    💳💺 Checkpoint ./scrapper.js:5328502/643

--- scrapper.js
+++ scrapper.js
@@ -6,13 +6,24 @@ const url = "https://blog.bitsrc.io/https-blog-bitsrc-io-how-to-perform-web-scra
 axios.get(url)
   .then( data => {
     let fData = [];
-    let og = fData.og;
+    let si = fData.site;
+    let og = fData.o_g;
     let tw = fData.twitter;
     const $ = cheerio.load(data.data);
-    fData.title = $('title').text();
-    og.title = $("meta[property='description']").attr("content");
+    fData.sitename = $("meta[property='og:site_name']").attr("content");
+//   Normal
+    fData.site.title = $('title').text();
+    fData.site.description = $("meta[name='description']").attr("content");
+  
+//     OG
+    og.title = $("meta[property='og:title']").attr("content");
+    og.description = $("meta[property='og:description']").attr("content");
+  
+//   Twitter Card
     tw.title = $("meta[property='twitter:title']").attr("content");
-    console.log(fData.description)
+    tw.description = $("meta[property='twitter:description']").attr("content");
+  
+    console.log(fData)
   })
   .catch( err => {
     console.log(err)
