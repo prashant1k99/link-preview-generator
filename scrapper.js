95b85231343fdfd6e0476eedcd77f8532e4d16fa
Glitch (hello-express) <none>
2019-11-28 09:11:57 +0000

    🏔🌋 Checkpoint ./scrapper.js:5328502/321

--- scrapper.js
+++ scrapper.js
@@ -1,10 +1,15 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 const url = "https://blog.bitsrc.io/https-blog-bitsrc-io-how-to-perform-web-scraping-using-node-js-5a96203cb7cb"
 
 axios.get(url)
   .then( data => {
-    console.log(data)
+    let fData = [];
+    const $ = cheerio.load(data.data);
+    fData.title = $('title').text();
+    fData.description = $('')
+    console.log(fData.title)
   })
   .catch( err => {
     console.log(err)
