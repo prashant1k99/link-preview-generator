81a676ebf96417b0d00fa7b51e4e9eb167dfe3c3
Glitch (hello-express) <none>
2019-11-29 07:19:54 +0000

    🙎🍯 Checkpoint ./scrapper.js:5328502/561

--- scrapper.js
+++ scrapper.js
@@ -3,6 +3,10 @@ const cheerio = require("cheerio");
 
 // const url = "https://blog.bitsrc.io/https-blog-bitsrc-io-how-to-perform-web-scraping-using-node-js-5a96203cb7cb"
 
+const validateURL = url => {
+  ^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$
+}
+
 const scrape = url => {
   return axios
     .get(url)
@@ -15,31 +19,29 @@ const scrape = url => {
         type: "Simple",
         title: $("title").text(),
         description: $("meta[name='description']").attr("content"),
-        icon: '',
-        image: ''
+        icon: $("link[rel='icon']").attr("href")
       };
       //     OG
       let og = {
         type: "OG",
         title: $("meta[property='og:title']").attr("content"),
-        description: $("meta[property='og:description']").attr("content")
+        description: $("meta[property='og:description']").attr("content"),
+        image: $("meta[property='og:image']").attr("content")
       };
       //   Twitter Card
       let tw = {
         type: "Twitter Card",
         title: $("meta[property='twitter:title']").attr("content"),
-        description: $("meta[property='twitter:description']").attr("content")
+        description: $("meta[property='twitter:description']").attr("content"),
+        image: $("meta[property='twitter:image:src']").attr("content")
       };
-
       fData.push(site);
       fData.push(og);
       fData.push(tw);
       return(fData);
     })
     .catch(err => {
-      console.log(err);
+      return(err);
     });
 };
 module.exports = scrape;
-
-// export default scrape;
