6051be716daaf6d1447c681ca1ac72ea60456bb8
Glitch (hello-express) <none>
2019-11-29 06:43:53 +0000

    💿🙈 Checkpoint ./index.js:5328502/53

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 const scrape = require("./scrapper.js")
 
 app.get("/", async (req, res) => {
-    let url = req.query.
+    // let url = req.query.
     let data = await scrape("https://blog.bitsrc.io/https-blog-bitsrc-io-how-to-perform-web-scraping-using-node-js-5a96203cb7cb")
     console.log(data);
     // res.send('Hello')
